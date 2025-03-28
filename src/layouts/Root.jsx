8776db67eac9d361f209a1ced898e12a43d1916e
jnakht <jisan.cse.cu@gmail.com>
2025-03-29 01:57:22 +0600

    basic layout for homepage

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div className='font-poppins'>
+        <div className='font-poppins max-w-6xl mx-auto'>
             <Outlet></Outlet>
         </div>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '../Shared/Header/Header';
 import Navbar from '../Shared/Navbar/Navbar';
+import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../Shared/RightSideNav/RightSideNav';
 
 const Home = () => {
     return (
@@ -8,6 +10,17 @@ const Home = () => {
             <Header></Header>
             <Navbar></Navbar>
             <h3 className="text-3xl font-poppins font-bold">This is home</h3>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+                <div className='border'>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className='md:col-span-2 border'>
+                    <p>News Coming Soon...</p>
+                </div>
+                <div className='border'>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
