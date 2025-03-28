8aca768e219daf73818c2c5e0d726e0db80e4fbc
jnakht <jisan.cse.cu@gmail.com>
2025-03-29 01:43:58 +0600

    header, navbar component created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../Shared/Header/Header';
+import Navbar from '../Shared/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h3 className="text-3xl font-poppins font-bold">This is home</h3>
         </div>
     );
