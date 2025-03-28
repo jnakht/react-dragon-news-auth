e9297e6042e329c8e7c6416f0499e27e21784d1b
jnakht <jisan.cse.cu@gmail.com>
2025-03-28 20:50:52 +0600

    applying font style in root

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h3 className="text-3xl">This is home</h3>
+            <h3 className="text-3xl font-poppins font-bold">This is home</h3>
         </div>
     );
 };
