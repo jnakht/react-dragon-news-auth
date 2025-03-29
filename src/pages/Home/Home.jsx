3e9af4e6b54b95d6894c8987526353edba32ff67
jnakht <jisan.cse.cu@gmail.com>
2025-03-29 14:05:35 +0600

    breaking news marquee added from react fast marquee

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import Header from '../Shared/Header/Header';
 import Navbar from '../Shared/Navbar/Navbar';
 import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
 import RightSideNav from '../Shared/RightSideNav/RightSideNav';
+import BreakingNews from './BreakingNews';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <h3 className="text-3xl font-poppins font-bold">This is home</h3>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,8 +2,8 @@ import moment from 'moment/moment';
 import logo from '../../../assets/logo.png'
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='text-center'>
+            <img className='mx-auto' src={logo} alt="" />
             <h3>Journalism Without Fear or Favour</h3>
             <p>{moment().format("dddd, MMMM Do, YYYY")}</p>
         </div>
