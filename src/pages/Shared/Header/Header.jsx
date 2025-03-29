6e8ba969adc357528fc548ca1bdac0bde8ca513b
jnakht <jisan.cse.cu@gmail.com>
2025-03-29 13:44:41 +0600

    working on header, dynamic date time added by momment js

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,8 +1,11 @@
-
+import moment from 'moment/moment';
+import logo from '../../../assets/logo.png'
 const Header = () => {
     return (
         <div>
-            <h3>This is header</h3>
+            <img src={logo} alt="" />
+            <h3>Journalism Without Fear or Favour</h3>
+            <p>{moment().format("dddd, MMMM Do, YYYY")}</p>
         </div>
     );
 };
